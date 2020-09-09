9c47b81b46ed327321cb79c823702b2f5f4e7972
areye022 <areye022@gmail.com>
2020-09-08 22:49:27 -0700

    created add event buttons

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ var workHours=['9AM','10AM','11AM','12PM',"1PM",'2PM','3PM','4PM','5PM']
 var todoList="";
 
 for (var i=0; i<workHours.length;i++){
-    console.log(workHours);
-    var listEl=document.createElement('input');
+    // to loop through the hours 
+    var hours=workHours[i]
+    // creating input elements to allow user to input plans for the day
+    var listEl=document.createElement('li');
+    // setting attributes
     listEl.setAttribute("id","planInput");
-    listEl.textContent=workHours[i];
+    // text Content
+    listEl.textContent=hours;
+    // appending 
     timeBlocks.appendChild(listEl);
+
+    // creating add buttons 
+    var addBtn=document.createElement('button')
+    addBtn.textContent='Add Event'
+    listEl.appendChild(addBtn);
 }
