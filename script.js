22a9ff919f67e3725db241e87a1b22f8093914a0
areye022 <areye022@gmail.com>
2020-09-10 14:08:54 -0700

    added all time blocks to html

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-// I am creating a work scheduler. it will show all hours from 9am-5pm
-// Each hour will have the option to add text content
-// when i add the text content, it will save to the local storage so that I may reload it
-// and it still be there.
-// The past times will be grey, present, green, and future yellow
+//insert date
 
-// 
-var currentDay=document.querySelector("#currentDay");
-var now=moment().format('MMMM Do YYYY');
-currentDay.innerHTML=now;
+var input = 
+
+window.onload = function(){
+    var day = document.getElementById("currentDay");
+    var now = moment().format("MMMM Do YYYY");
+    day.innerHTML = now
+    
+}
+
+$('#resetBtn').on('click', function(){
+    $('.reset').innerHTML="";
+})
+
+// single function, filter using ID of the target element 
 
 
-// creating a practice box to append     
-var timeBlocks= document.querySelector(".container")
+//save
+$("#saveBtn").on("click",function(){
+    
 
-
-// var workHours=['9AM','10AM','11AM','12PM',"1PM",'2PM','3PM','4PM','5PM']
-// blank var to add inputs 
-$("#saveBtn").on("click", function(){
-    var input = document.getElementById("fiveAmTask");
-    // console.log(‘This is the input ->’)s
+    // console.log('This is the input ->')s
     console.log(input)
-    var content=input.value;
-    // console.log(‘This is the content ->’)
+
+    var content = input.value;
+
+    //console.log('This is the content ->')
     console.log(content)
-    localStorage.setItem('storedStuff', JSON.stringify(content));
-    var storedInput = localStorage.getItem('storedStuff')
-    console.log("This is the stored input ->", JSON.parse(storedInput));
+
+    localStorage.setItem("storedStuff", JSON.stringify(content));
+
 });
 
+//colors
+// give a id to each input
 
